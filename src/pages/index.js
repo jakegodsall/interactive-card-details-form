31bae4664ed6954e532bfa2248775619bb031583
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-14 07:07:43 +0000

    changed grid layout in desktop layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,9 @@ const Home = () => {
 
     return (
         <React.Fragment>
-            <div className='absolute w-screen h-[30vh] bg-mainBackgroundMobile md:bg-mainBackgroundDesktop bg-no-repeat bg-cover'></div>
-            <div className='w-screen h-screen flex flex-col'>
-                <div className='grid grid-rows-[auto_1fr] gap-10 mt-10'>
+            <div className='absolute w-screen h-[30vh] bg-mainBackgroundMobile bg-no-repeat bg-cover lg:bg-mainBackgroundDesktop lg:w-[30vw] lg:h-screen'></div>
+            <div className='w-screen h-screen flex flex-col lg:justify-center transition-all duration-1000'>
+                <div className='grid grid-rows-[auto_1fr] gap-10 mt-10 lg:grid-rows-1 lg:grid-cols-2'>
                     <Card formData={formHandler} />
                     <Form onHandleForm={formHandler} />
                 </div>
