dd813539555809090a6a42c633a5ffe9f56115de
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-14 07:14:10 +0000

    added max-width to grid in desktop view

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const Home = () => {
         <React.Fragment>
             <div className='absolute w-screen h-[30vh] bg-mainBackgroundMobile bg-no-repeat bg-cover lg:bg-mainBackgroundDesktop lg:w-[30vw] lg:h-screen'></div>
             <div className='w-screen h-screen flex flex-col lg:justify-center transition-all duration-1000'>
-                <div className='grid grid-rows-[auto_1fr] gap-10 mt-10 lg:grid-rows-1 lg:grid-cols-2'>
+                <div className='grid grid-rows-[auto_1fr] gap-10 mt-10 lg:grid-rows-1 lg:grid-cols-2 lg:mx-auto lg:gap-[10rem]'>
                     <Card formData={formHandler} />
                     <Form onHandleForm={formHandler} />
                 </div>
