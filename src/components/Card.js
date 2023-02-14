3dd789f4bbfaf9db33328afc6642bde84d566d18
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-14 10:51:05 +0000

    added leading zero formatting for cvc

--- src/components/Card.js
+++ src/components/Card.js
@@ -48,11 +48,22 @@ const Card = (props) => {
         return formattedStr;
     };
 
+    const formatCVC = (cvc) => {
+        let cvcArray = ['0', '0', '0'];
+
+        for (let i = 0; i < cvc.trim().length; i++) {
+            cvcArray.shift();
+            cvcArray.push(cvc[i]);
+        }
+
+        return cvcArray.join('');
+    };
+
     return (
         <div className='mx-auto w-full max-w-[450px] h-auto relative py-40 px-2 flex flex-col items-center justify-center md:py-44 lg:h-[500px]'>
             <div className='absolute w-[330px] max-w-[400px] aspect-[11/6] bg-cardBack bg-no-repeat bg-cover right-2 top-2 rounded-md md:w-[400px] md:-right-14 lg:bottom-2 lg:top-auto transition-all duration-1000'>
                 <p className='absolute tracking-widest top-[5rem] right-12 text-white text-xs text-[#fff] md:top-[6.25rem] transition-all duration-1000'>
-                    {formData.cvc ? formData.cvc : '000'}
+                    {formData.cvc ? formatCVC(formData.cvc) : '000'}
                 </p>
             </div>
             <div className='absolute w-[330px] max-w-[400px] aspect-[11/6] bg-cardFront bg-no-repeat bg-cover left-2 bottom-2 rounded-md flex flex-col items-center p-6 text-[#fff] md:w-[400px] lg:top-2 lg:bottom-auto transition-all duration-1000'>
