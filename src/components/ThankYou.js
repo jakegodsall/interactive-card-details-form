fcaffd2a1f6aab1294aab650d018ce23882718ec
jakegodsall <jake.edward.godsall@gmail.com>
2023-02-14 17:52:31 +0000

    fixed layout for ThankYou in the desktop viewport

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -12,7 +12,7 @@ const ThankYou = (props) => {
     };
 
     return (
-        <div className='mx-auto px-4 flex flex-col items-center max-w-[450px]'>
+        <div className='mx-auto px-4 w-full flex flex-col gap-8 items-center max-w-[450px] lg:justify-center lg:w-[700px]'>
             <Image src={CompleteIcon} alt='form data submitted' />
             <h1 className='uppercase tracking-widest text-3xl'>Thank You!</h1>
             <p className='text-dark-grey-violet tracking-wide font-bold'>
